0bb2424f6e5b7f106548a852a52cb83e2008050c
Jordydwd <jordyromero354@gmail.com>
2025-10-02 13:44:38 -0500

    cambios

--- src/main/java/com/example/controller/UsuarioController.java
+++ src/main/java/com/example/controller/UsuarioController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.example.models.Usuario;
 import com.example.service.IUsuarioService;
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/usuario")
 public class UsuarioController {
