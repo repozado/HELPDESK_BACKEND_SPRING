06b249b5f450657ccd8fea14d250ee4749b2604e
repozado <joseridriguez65@gmail.com>
2025-10-02 01:29:11 -0500

    accesibilidad

--- src/main/java/com/example/controller/TicketPriorityController.java
+++ src/main/java/com/example/controller/TicketPriorityController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.example.models.TicketPriority;
 import com.example.models.TicketStatus;
 import com.example.service.ITicketPriorityService;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/ticketpriority")
 public class TicketPriorityController {
